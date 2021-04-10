8e75ed4d97859b4d808759034bb936f9bb29f8e2
soldatgg <67762736+soldatgg@users.noreply.github.com>
2021-04-11 00:52:22 +0200

    i need help that 1 am i need to get soem rest

--- RETRO/Assets/Script/CTNclipManag.cs
+++ RETRO/Assets/Script/CTNclipManag.cs
@@ -6,6 +6,10 @@ public class CTNclipManag : MonoBehaviour
 {
 
     public MeshRenderer TestMesh;
+    public GameObject ClipManager;
+    public bool TestClip;
+    public bool MoveClip;
+    public bool AnchorMoveClip;
 
     // Start is called before the first frame update
     void Start()
@@ -23,13 +27,13 @@ public class CTNclipManag : MonoBehaviour
 
     private void Unable()
     {
-
+        TestMesh.enabled = true;
 
     }
 
     private void Disable()
     {
-
+        TestMesh.enabled = false;
 
     }
 

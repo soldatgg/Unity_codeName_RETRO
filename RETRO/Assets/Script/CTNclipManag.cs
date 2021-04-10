5086786e1b1816e7b2ad4adf2d4527d00f8a7053
soldatgg <67762736+soldatgg@users.noreply.github.com>
2021-04-10 23:46:59 +0200

    Add a script for unable or disable clip viewveer

--- RETRO/Assets/Script/CTNclipManag.cs
+++ RETRO/Assets/Script/CTNclipManag.cs
@@ -2,17 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class CTNclipManag : MonoBehaviour
 {
+
+    public MeshRenderer TestMesh;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
     }
+
+    private void Unable()
+    {
+
+
+    }
+
+    private void Disable()
+    {
+
+
+    }
+
 }
